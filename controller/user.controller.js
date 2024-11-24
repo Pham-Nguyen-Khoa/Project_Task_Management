5f03e17c76002e95bf21a2240672d9f9b1bc9727
Pham Nguyen Khoa <khoapnse183214@fpt.edu.vn>
2024-11-24 20:35:57 +0700

    Forgot-Password

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,7 +1,8 @@
 const User = require("../models/user.model");
 const md5 = require("md5");
-
-
+const generateHelper = require("../helpers/generateHelper");
+const ForgotPassword = require("../models/forgot-password.model");
+const nodeMailerHelper = require("../helpers/nodemailer");
 
 /**
  * @swagger
@@ -52,7 +53,7 @@ const md5 = require("md5");
  *                   example: 400
  *                 message:
  *                   type: string
- *                   example: "Email không tồn tại hoac Mật khẩu không chính xác"  
+ *                   example: "Email không tồn tại hoac Mật khẩu không chính xác"
  */
 // [POST] localhost:3000/users/login
 module.exports.login = async (req, res) => {
@@ -80,7 +81,7 @@ module.exports.login = async (req, res) => {
     res.json({
       code: 200,
       message: "Đăng nhập thành công",
-      token: user.token
+      token: user.token,
     });
   } catch (error) {
     res.json({
@@ -177,6 +178,274 @@ module.exports.register = async (req, res) => {
     res.json({
       code: 400,
       message: "Tạo tài khoản thất bại",
+    });
+  }
+};
+
+/**
+ * @swagger
+ * /users/password/forgot:
+ *   post:
+ *     tags:
+ *       - Forgot-Password
+ *     summary: Quên mật khẩu
+ *     description: Gửi mã OTP đến email của người dùng để khôi phục mật khẩu.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Email của người dùng
+ *                 example: example@gmail.com
+ *     responses:
+ *       200:
+ *         description: Gửi OTP thành công.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: "Đã gửi OTP qua email"
+ *                 email:
+ *                   type: string
+ *                   example: example@gmail.com
+ *       400:
+ *         description: Lỗi trong quá trình gửi OTP hoặc email không tồn tại.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 400
+ *                 message:
+ *                   type: string
+ *                   example: "Email không tồn tại hoặc Lỗi trong quá trình khôi phục mật khẩu"
+ */
+// [POST] localhost:3000/users/password/forgot
+module.exports.forgot = async (req, res) => {
+  try {
+    const email = req.body.email;
+
+    const user = await User.findOne({
+      email: email,
+      deleted: false,
+    });
+    if (!user) {
+      return res.json({
+        code: 400,
+        message: "Email không tồn tại",
+      });
+    }
+
+    const otp = generateHelper.generateRandomNumber(6);
+    const forgotPassword = new ForgotPassword({
+      email: email,
+      otp: otp,
+      expireAt: new Date(),
+    });
+    await forgotPassword.save();
+    const descriptionHTML = ` <h1>Mã OTP của bạn là <b>${otp}</b></h1>`;
+    nodeMailerHelper.sendMail(email, "Khôi phục mật khẩu ", descriptionHTML);
+
+    res.json({
+      code: 200,
+      message: "Đã gửi OTP qua email",
+      email: email,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi trong quá trình khôi phục mật khẩu",
+    });
+  }
+};
+
+/**
+ * @swagger
+ * /users/password/otp:
+ *   post:
+ *     tags:
+ *       - Forgot-Password
+ *     summary: Xác thực mã OTP
+ *     description: Kiểm tra mã OTP do người dùng nhập và trả về token nếu thành công.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: Email của người dùng
+ *                 example: example@gmail.com
+ *               otp:
+ *                 type: string
+ *                 description: Mã OTP được gửi qua email
+ *                 example: "123456"
+ *     responses:
+ *       200:
+ *         description: Xác thực OTP thành công.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: "Xác thực thành công"
+ *                 token:
+ *                   type: string
+ *                   description: Token của người dùng
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR..."
+ *       400:
+ *         description: Lỗi trong quá trình xác thực OTP hoặc mã OTP không đúng.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 400
+ *                 message:
+ *                   type: string
+ *                   example: "Mã OTP không đúng! hoặc Lỗi trong quá trình khôi phục mật khẩu"
+ */
+// [POST] localhost:3000/users/password/otp
+module.exports.otp = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+    const forgotObject = await ForgotPassword.findOne({
+      email: email,
+      otp: otp,
+    });
+    if (!forgotObject) {
+      return res.json({
+        code: 400,
+        message: "Mẫ OTP không đúng!",
+      });
+    }
+    const user = await User.findOne({
+      email: email,
+      deleted: false,
+    });
+    res.cookie("token", user.token);
+    res.json({
+      code: 200,
+      message: "Xác thực thành công",
+      token: user.token,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi trong quá trình khôi phục mật khẩu",
+    });
+  }
+};
+
+/**
+ * @swagger
+ * /users/password/reset:
+ *   post:
+ *     tags:
+ *       - Forgot-Password
+ *     summary: Đặt lại mật khẩu
+ *     description: Đặt lại mật khẩu mới cho tài khoản dựa trên token xác thực.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               password:
+ *                 type: string
+ *                 description: Mật khẩu mới
+ *                 example: "newpassword123"
+ *               confirmPassword:
+ *                 type: string
+ *                 description: Xác nhận mật khẩu mới
+ *                 example: "newpassword123"
+ *     responses:
+ *       200:
+ *         description: Đổi mật khẩu thành công.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: "Đổi mật khẩu thành công"
+ *       400:
+ *         description: Lỗi khi đổi mật khẩu, có thể do token không hợp lệ, mật khẩu không khớp, hoặc lỗi hệ thống.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 400
+ *                 message:
+ *                   type: string
+ *                   example: "Mật khẩu không khớp! hoặc Đổi mật khẩu thất bại"
+ */
+// [POST] localhost:3000/users/password/reset
+module.exports.reset = async (req, res) => {
+  try {
+    const token = req.cookies.token
+    const {  password, confirmPassword } = req.body;
+    if (password != confirmPassword) {
+      return res.json({
+        code: 400,
+        message: "Mật khẩu không khớp!",
+      });
+    }
+    const user = await User.findOne({
+      token: token,
+    });
+    if (md5(password) == user.password) {
+      return res.json({
+        code: 400,
+        message: "Vui lòng nhập mật khẩu mới khác mật khẩu cũ",
+      });
+    }
+    await User.updateOne(
+      {
+        token: token,
+        deleted: false,
+      },
+      {
+        password: md5(password),
+      }
+    );
+    res.json({
+      code: 200,
+      message: "Đổi mật khẩu thành công",
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Đổi mật khẩu thất bại",
     });
   }
 };
--- router/users.router.js
+++ router/users.router.js
@@ -6,6 +6,13 @@ router.post("/login", userController.login);
 
 router.post("/register", userController.register);
 
+router.post("/password/forgot", userController.forgot);
+
+router.post("/password/otp", userController.otp);
+
+
+router.post("/password/reset", userController.reset);
+
 
 
 module.exports = router;
